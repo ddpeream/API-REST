b8c3e433c3464bea8f577dee3d853346c5440307
deimar danubio perea moreno <ddpeream@unal.edu.co>
2022-12-20 23:39:59 -0500

    act 1

--- index.js
+++ index.js
@@ -7,19 +7,21 @@ const app = express();
 app.use(express.json());
 
 /* Puerto quemado en 3000 */
-const {config} = require('./config/index')
+const {config} = require('./config/index');
+const { logErrors, wrapErrors, errorHandler } = require('./utils/middleware/errorHandles');
+// const { notFoundHandler } = require('./utils/middleware/notFoundHandler')
 
 /* Servidor con CRUD(get, post, put, update, delete) */
 moviesApi(app)
 
 /* Manejo del error 404 */
-//app.use(notFoundHandler)
+// app.use(notFoundHandler)
 
 
 /* Capa de middleware (manejo de errores) */
-//app.use(logErrors)
-//app.use(wrapErrors)
-//app.use(errorHandler)
+app.use(logErrors)
+app.use(wrapErrors)
+app.use(errorHandler)
 
 /* Escuchando servidor */
 app.listen(config.port, () =>console.log('http://localhost:3000: Backend of movies'))
